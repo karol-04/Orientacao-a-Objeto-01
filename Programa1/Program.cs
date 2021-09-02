637352c0b4ebf1b6d27819f74f484a72e437f63f
Karolaine <karolbt04@gmail.com>
2021-09-02 09:59:09 -0300

    Adicionando primeiro teste do terminal

--- Programa1/Program.cs
+++ Programa1/Program.cs
@@ -6,7 +6,7 @@ namespace Programa1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mensagem importante!");
         }
     }
 }
